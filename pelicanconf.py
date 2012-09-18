1595fed79af54769b7e4588be9fcac1836a115ff
Dominique Lederer <dominique.lederer@wunderweiss.com>
2012-09-18 10:46:15 +0200

    pep8 adaptions

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,19 +12,20 @@ TIMEZONE = 'Europe/Vienna'
 DEFAULT_LANG = 'en'
 
 # Social widget
-SOCIAL = (('twitter', 'https://twitter.com/return1_at'),
-          ('google+', 'https://plus.google.com/113955961996209596236/about'),
-          ('github', 'https://github.com/return1'),
-          ('linkedin', 'http://at.linkedin.com/in/dominiquelederer'),
-          ('xing', 'https://www.xing.com/profile/Dominique_Lederer'),
-          ('about.me', 'https://about.me/dominique.lederer/'),
-    )
+SOCIAL = (
+    ('twitter', 'https://twitter.com/return1_at'),
+    ('google+', 'https://plus.google.com/113955961996209596236/about'),
+    ('github', 'https://github.com/return1'),
+    ('linkedin', 'http://at.linkedin.com/in/dominiquelederer'),
+    ('xing', 'https://www.xing.com/profile/Dominique_Lederer'),
+    ('about.me', 'https://about.me/dominique.lederer/'),
+)
 
 DEFAULT_PAGINATION = 5
 
 THEME = "themes/return1"
 
-STATIC_PATHS = ['images','assets','sandbox']
+STATIC_PATHS = ['images', 'assets', 'sandbox']
 
 LOCALE = 'en_US.UTF-8'
 DEFAULT_DATE_FORMAT = '%B %d, %Y'
@@ -35,7 +36,7 @@ SUMMARY_MAX_LENGTH = 30
 FILES_TO_COPY = (
     ('assets/robots.txt', 'robots.txt'),
     ('assets/favicon.ico', 'favicon.ico'),
-    )
+)
 
 DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives', 'sitemap')
 SITEMAP_SAVE_AS = 'sitemap.xml'
